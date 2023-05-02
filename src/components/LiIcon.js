f8161c37590444b0d34e0f346445cf7cade2e096
Berhanu Kebito <brhankbt63@gmail.com>
2023-05-02 21:27:18 +0300

    To be deployed

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -8,7 +8,7 @@ const {scrollYProgress}=useScroll({
     offset:["center end","center center"]
 })
   return (
-   <figure className='absolute left-0 stroke-dark dark:stroke-light'>
+   <figure className='absolute left-0 stroke-dark dark:stroke-light '>
     <svg className='-rotate-90 -mt-[85px] xs:-mt-[130px] md:-mt-[75px] xs:-ml-[30px] md:w-[60px] md:h-[60px] xs:[40px]'  width="75" height="75" viewBox='0 0 100 100'>
         <circle cx='75' cy='50' r='20' className='stroke-primary dark:stroke-primaryDark stroke-1 fill-none'/>
         <motion.circle cx='75' cy='50' r='20' className='stroke-[5px] fill-light dark:fill-dark'
