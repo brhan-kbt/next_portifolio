48333de42c500d9c8671c33502cf70e56646dbfc
Berhanu Kebito <brhankbt63@gmail.com>
2023-12-11 11:09:36 +0300

    updated projects

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,9 +21,9 @@ const AnimatedNumbers=({value})=>{
     useEffect(() => {
         if (isInView) {
             motionValue.set(value)
-        }     
+        }
     }, [isInView,value,motionValue])
-    
+
 
     useEffect(()=>{
         springValue.on('change',(latest)=>{
@@ -32,7 +32,7 @@ const AnimatedNumbers=({value})=>{
             }
         })
     },[springValue,value])
-   
+
     return <span ref={ref}></span>
 }
 
@@ -55,14 +55,14 @@ const about = () => {
 
                     , a skilled software developer with experience in a variety of programming
                      languages and frameworks. My technical expertise includes C++, Java, C#, HTML, CSS, JavaScript,
-                      PHP, Laravel, SQL server, MYSQL, Bootstrap, React, Angular,Nextjs, Nodejs and WordPress. 
-                      Additionally, I'm proficient in design using Figma, enabling me to deliver visually 
+                      PHP, Laravel, SQL server, MYSQL, Bootstrap, React, Angular,Nextjs, Nodejs and WordPress.
+                      Additionally, I'm proficient in design using Figma, enabling me to deliver visually
                       appealing web solutions that engage users.
                     </p>
                    <br/>
                     <p className='font-sm text-justify'>
-                        My passion for web development drives me to stay up-to-date with the latest advancements 
-                        in the field, ensuring that I provide my clients with the most innovative solutions possible. 
+                        My passion for web development drives me to stay up-to-date with the latest advancements
+                        in the field, ensuring that I provide my clients with the most innovative solutions possible.
                         With my strong work ethic, keen eye for detail, and client-focused approach, I'm confident that
                          I can excel as a web developer and contribute my skills and experience to any organization.
                     </p>
@@ -74,7 +74,7 @@ const about = () => {
                 bg-light dark:bg-dark dark:border-light p-8'>
                     <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'/>
                     <Image src={profilePic} alt='BIRHANU KABITO' className='w-full h-auto rounded-2xl'
-                     priority 
+                     priority
                      sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw,33vw'
                     />
                 </div>
@@ -88,11 +88,11 @@ const about = () => {
                     </div>
                     <div className='flex flex-col items-end justify-center  xl:items-center'>
                         <span className='inline-block text-7xl font-bold md:text-5xl sm:text-4xl xs:text-3xl'>
-                        <AnimatedNumbers value={5}/>+
+                        <AnimatedNumbers value={9}/>+
                         </span>
                         <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>Projects Completed</h2>
                     </div>
-                    
+
                     <div className='flex flex-col items-end justify-center  xl:items-center'>
                         <span className='inline-block text-7xl font-bold md:text-5xl sm:text-4xl xs:text-3xl'>
                         <AnimatedNumbers value={1}/>+
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,7 +6,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import yes from '../../public/images/projects/yes.png'
-import eonline from '../../public/images/projects/eonline.png'
+import eonline from '../../public/images/projects/eaglopia.PNG'
+import keradion from '../../public/images/projects/keradion.PNG'
 import eicad from '../../public/images/projects/eicad.png'
 import eyooad from '../../public/images/projects/eyooad.png'
 import eyooweb from '../../public/images/projects/eyoo3.png'
@@ -18,14 +19,14 @@ const FramerImage=motion(Image)
 const FeaturedProject=({type,title,summary,img,link,github})=>{
     return(
 
-        <article className='w-full p-12 flex items-center 
+        <article className='w-full p-12 flex items-center
         justify-between rounded-3xl border border-solid
         border-dark bg-light shadow-2xl relative
         rounded-br-2xl dark:bg-dark dark:border-light
         lg:flex-col ld:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4
         '>
             <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]'/>
-           
+
             <Link href={link} target='_blank'
             className='w-1/2 lg:w-full cursor-pointer overflow-hidden rounded-lg '
             >
@@ -33,7 +34,7 @@ const FeaturedProject=({type,title,summary,img,link,github})=>{
                  whileHover={{scale:1.05}}
                  transition={{duration:.2}}
                 src={img} alt={title} className='w-full h-auto'
-                priority 
+                priority
                 sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw,50vw'
                 />
             </Link>
@@ -62,7 +63,7 @@ const Project=({title,type,img,link,github})=>{
              xs:p-4
             '
             />
-             
+
               <Link href={link} target='_blank'
             className='w-full inline-block cursor-pointer overflow-hidden rounded-lg md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]'
             >
@@ -77,11 +78,11 @@ const Project=({title,type,img,link,github})=>{
                     <h2 className='my-2 w-full text-left text-2xl font-bold lg:text-2xl'>{title}</h2>
                 </Link>
                 <div className='mt-2 flex items-center justify-between w-full'>
-                    <Link href={link} target='_blank' 
+                    <Link href={link} target='_blank'
                         className='text-lg font-semibold underline underline-offset-2 md:text-base'>Visit
                     </Link>
                     <Link className='w-8 md:w-8' href={github} target='_blank'><GithubIcon/></Link>
-                   
+
                 </div>
             </div>
         </article>
@@ -108,86 +109,108 @@ const projects = () => {
                     summary="
                     Developed using WordPress, Superio theme, Bootstrap, Elementor, social login, and other technologies provides a
                      feature-rich platform for job seekers and employers. It allows users to search for
-                      job vacancies, create job profiles, and apply for jobs. Additionally, it includes social 
+                      job vacancies, create job profiles, and apply for jobs. Additionally, it includes social
                       login features for easy access, and its responsive design ensures that users can access the portal from any device"
                     github='/'
                     type='Featured Project'
                     img={yes}
-                   
+
                    />
                 </div>
                 <div className='col-span-6 sm:col-span-12'>
                 <Project
                    title='Eyoo fun Website'
                    summary="
-                    A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                    It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                    A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts.
+                    It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your
                     local currency."
                     link='https://eyoofun.netlify.app/'
                     github='/'
                     type='Featured Project'
                     img={eyooweb}
-                   
-                   />                
+
+                   />
                 </div>
                 <div className='col-span-6 sm:col-span-12'>
                     <Project
                     title='Gadaa News ~ Blog Website'
                     summary="
-                        A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                        A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts.
+                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your
                         local currency."
-                        link='www.gadanews.com'
+                        link='https://gadanews.com'
                         github='/'
                         type='Featured Project'
                         img={gadaa}
-                    
-                    />    
+
+                    />
                 </div>
                 <div className='col-span-12'>
                     <FeaturedProject
                     title='EIC Call Center Directory Listing Website'
-                    link='https://www.eagleinformationcenter.com'
+                    link='https://www.eaglopia.com'
                     summary="
-                    The EIC Directory Listing Website is a robust web 
-                    application developed using 
+                    The EIC Directory Listing Website is a robust web
+                    application developed using
                     Laravel and React. This cutting-edge platform
-                     provides an extensive directory of businesses 
-                     and services in Ethiopia, making it easy for users 
+                     provides an extensive directory of businesses
+                     and services in Ethiopia, making it easy for users
                      to find what they need quickly and efficiently."
                         github='/'
                         type='Featured Project'
                         img={eonline}
-                    
-                    />                
+
+                    />
                 </div>
                 <div className='col-span-6 sm:col-span-12'>
                     <Project
                     title='EIC Call Center Application'
                     summary="
-                        A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                        A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts.
+                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your
                         local currency."
-                        link='/'
+                        link='#'
                         github='/'
                         type='Featured Project'
                         img={eicad}
-                    
-                    />    
+
+                    />
                 </div>
                 <div className='col-span-6 sm:col-span-12'>
                     <Project
                     title='Eyoo Admin Dashboard'
                     summary="
-                        A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+                        A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts.
+                        It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your
                         local currency."
-                        link='/'
+                        link='#'
                         github='/'
                         type='Featured Project'
                         img={eyooad}
-                    
-                    />    
+
+                    />
+                </div>
+
+                <div className='col-span-12'>
+                    <FeaturedProject
+                    title='Keradion Designs Solutions'
+                    link='https://www.keradiondesigns.com'
+                    summary="Keradion Designs, the preferred partner
+                    for top Ethiopian enterprises, specializes in bespoke
+                     software development, innovative product design,
+                     and expert consultancy.
+
+                    Keradion Designs is dedicated to delivering
+                    tailored solutions that enhance business value
+                     and showcase their cutting-edge technological
+                      expertise.
+
+                   "
+                        github='/'
+                        type='Featured Project'
+                        img={keradion}
+
+                    />
                 </div>
             </div>
         </Layout>
